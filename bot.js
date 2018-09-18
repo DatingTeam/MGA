da4cba5792887ffa2652b3b61c78dea4bd336bbb
DatingTeam <43161017+DatingTeam@users.noreply.github.com>
2018-09-18 08:48:13 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -111,29 +111,8 @@ client.on('message', async message => {
     } 
  });
 
-client.on('message', message => {
-    if (message.author.id === client.user.id) return;
-    if (message.guild) {
-   let embed = new Discord.RichEmbed()
-    let args = message.content.split(' ').slice(1).join(' ');
-if(message.content.split(' ')[0] == prefix + 'bc') {
-    if (!args[1]) {
-return;
-}
-        message.guild.members.forEach(m => {
-   if(!message.member.hasPermission('ADMINISTRATOR')) return;
-            var bc = new Discord.RichEmbed()
-            .addField(' » message : ', args)
-            .setFooter('Dating Team')
-            .setColor('#ff0000')
-            
-            m.send(`${m}`,{embed: bc});
-        });
-    }
-    } else {
-        return;
-    }
-});
+
+      
 
 
 
